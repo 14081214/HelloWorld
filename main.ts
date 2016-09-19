eeb39e8a12aa25e3c118e3a9a7c9fd0673b70efd
Ellali <lyh3109@sina.cn>
2016-09-20 00:58:45 +0800

    It's too late...tomorrow going on...

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("hahaha");
+function pyramid(n:number):void{
+    var k:string = " ";
+    var x:string = "*";
+    for(var i=0;i<n;i++){
+        for(var j=0;j<n-i-1;j++){
+            console.log(k);
+        }
+        for(var z=0;z<i;z++){
+            console.log(x);
+        }
+    }
+}
+//var a = parseInt(n);
+pyramid(5);
