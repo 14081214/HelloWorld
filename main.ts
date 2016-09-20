88089a4170b54ca00917ad114c77ac8db34e7d21
Ellali <lyh3109@sina.cn>
2016-09-20 12:50:06 +0800

    终于。。。

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 
 function pyramid(n:number):void{
-    var w:string = "";
+    //var w:string = "";
     //var k:string = " ";
     //var x:string = "*";
     for(var i=0;i<n;i++){
+        var w:string = "";
         for(var j=0;j<n-i-1;j++){
            // console.log("l");
            w = w+" ";
         }
-        for(var z=0;z<i;z++){
+        for(var z=0;z<2*i+1;z++){
            // console.log("*");
            w = w+"*";
         }
+        //console.log("\n");
+        console.log(w);
     }
-    console.log(w);
+    //console.log(w);
 }
 //var a = parseInt(n);
-pyramid(5);
+pyramid(7);
