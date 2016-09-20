7091bf5850ae62c3837ae33cb04f5e8b150f7d1d
Ellali <lyh3109@sina.cn>
2016-09-21 00:30:47 +0800

    Finish Homework 2

--- main.ts
+++ main.ts
@@ -1,22 +1,17 @@
-
 function pyramid(n:number):void{
     //var w:string = "";
-    //var k:string = " ";
-    //var x:string = "*";
     for(var i=0;i<n;i++){
         var w:string = "";
         for(var j=0;j<n-i-1;j++){
-           // console.log("l");
            w = w+" ";
         }
         for(var z=0;z<2*i+1;z++){
-           // console.log("*");
            w = w+"*";
         }
         //console.log("\n");
         console.log(w);
     }
-    //console.log(w);
 }
-//var a = parseInt(n);
-pyramid(7);
+//var x:string = process.argv[2];
+//var y:number = parseInt(x);
+pyramid(8);
